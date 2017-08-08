61151417df037379c21e021d0e5ad867d5aa9359
Matthew Peixoto <matthewpeixoto@gmail.com>
2017-08-08 14:37:46 -0400

    Complete project

---
+++ js/contacts.js
@@ -0,0 +1,20 @@
+var peeps = [
+  {
+    name: 'Mercury Man',
+    email: 'theman@mercury.com',
+    tel: '+05551234',
+    loc: [46001200, 69816900]
+  },
+  {
+    name: 'Venus ’Venturer',
+    email: 'venus@theadventurer.com',
+    tel: '+15551234',
+    loc: [107477000, 108939000]
+  },
+  {
+    name: 'Jupiter Juggernaut',
+    email: 'thejugger@jupiter.com',
+    tel: '+55551234',
+    loc: [816040000, 740550000]
+  }
+];
---
+++ js/main.js
@@ -0,0 +1,17 @@
+var listContacts = function (contacts) {
+  document.write('<h1>Planetary peeps</h1>');
+
+  contacts.forEach(function (item) {
+    document.write('<h2>' + item.name + '</h2>');
+    document.write('<dl>');
+    document.write('<dt>E-mail address</dt>');
+    document.write('<dd><a href="mailto:' + item.email + '">' + item.email + '</a></dd>');
+    document.write('<dt>Phone number</dt>');
+    document.write('<dd><a href="tel:' + item.tel + '">' + item.tel + '</a></dd>');
+    document.write('<dt>Location</dt>');
+    document.write('<dd>Between ' + item.loc[0] + ' km & ' + item.loc[1] + ' km from the Sun.</dd>');
+    document.write('</dl>');
+  });
+};
+
+listContacts(peeps);
